bc911a7ee9c1999c4d3c4784787250f4968abbe4
Leon-Carlo <carloraul826@gmail.com>
2025-03-19 12:01:04 -0500

    Update 1vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
   build: {
-    outDir: 'dist',  // Asegúrate de que 'dist' esté configurado como salida
+    outDir: 'public/build',  // Especifica que la salida será en 'public/build'
   },
-};
 });
